d58c35d67c15c230ab45f52cc6a5fbe79ab3d486
flyingPie23 <drissfreah325@gmail.com>
2025-07-25 22:45:17 +0100

    added the model and controller to track each search query

---
+++ test/controllers/request_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class RequestControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get request_create_url
+    assert_response :success
+  end
+end
