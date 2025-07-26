f31e727f8c3b9f304a9b3f9fcd2195a02172ecc2
flyingPie23 <drissfreah325@gmail.com>
2025-07-26 18:19:38 +0100

    added graphs on analytic page

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem "bootstrap"
 gem 'faker'
 
 gem "chartkick"
+gem "groupdate"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.min.js", preload: true
+
+pin "chartkick", to: "chartkick.js"
+pin "Chart.bundle", to: "Chart.bundle.js"
